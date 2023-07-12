1fe5a2d5bcdd29f8b1bd263d97ffbc0c68970c0a
Alishah Safdari <alishahsafdari@gmail.com>
2023-07-12 07:06:47 +0430

    solve the conflict

--- data.sql
+++ data.sql
@@ -1,13 +1,9 @@
 /* Populate database with sample data. */
-<<<<<<< HEAD
 
-=======
->>>>>>> 0f33b7a865a3bfa02f4488254f9b555e1845abe1
 INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg )VALUES (1,'Agumon','2020-2-3',0,true,10.23);
 INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (2,'Gabumon','2018-11-15',2,true,8);
 INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (3,'Pikachu','2021-1-7',1,false,15.04);
 INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (4,'Devimon','2017-5-12',5,true,11);
-<<<<<<< HEAD
 /*INsert new data*/
 INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (5,'Charmander','2020-2-8',0,false,-11);
 INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (6,'Plantmon','2021-11-15',3,true,-5.7);
@@ -16,5 +12,3 @@ INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) V
 INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (9,'Boarmon','2005-01-7',7,true,20.4);
 INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (10,'Blossom','1998-10-13',3,true,17);
 INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (11,'Ditto','2022-05-14',4,true,22);
-=======
->>>>>>> 0f33b7a865a3bfa02f4488254f9b555e1845abe1
