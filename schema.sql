57fbc09ea6d617b4134afbc7618b091c199a54df
Alishah Safdari <alishahsafdari@gmail.com>
2023-07-12 06:55:41 +0430

    Add new column in animals table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,13 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+CREATE TABLE animals(
+   id INT PRIMARY KEY     NOT NULL,
+   name           varchar(100)    NOT NULL,
+   date_of_birth     date            NOT NULL,
+   escape_attempts  INT,
+   neutered   boolean,
+   weight_kg  decimal(5,2),
 );
+
+/* Add species column in animals table*/
+ALTER TABLE animals ADD species VARCHAR(150);
