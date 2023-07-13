b080b701b6d4ba4cd453239619a7203ed38431b6
Alishah Safdari <alishahsafdari@gmail.com>
2023-07-13 23:10:04 +0430

    Add data in tables

--- data.sql
+++ data.sql
@@ -34,5 +34,68 @@ Melody Pond owns Charmander, Squirtle, and Blossom.
 Dean Winchester owns Angemon and Boarmon.
 */
 UPDATE animals SET owner_id = 1 WHERE name = 'Agumon';
-UPDATE animals SET owner_id = 2 WHERE name = 'Gabumon' OR name ='Pikachu' ;
+UPDATE animals SET owner_id = 2 WHERE name = 'Gabumon' OR name ='Pikachu';
+UPDATE animals SET owner_id = 3 WHERE name = 'Devimon' OR name ='Plantmon';
+UPDATE animals SET owner_id = 4 WHERE name = 'Charmander' OR name ='Squirtle' OR name='Blossom';
+UPDATE animals SET owner_id = 5 WHERE name = 'Angemon' OR name ='Boarmon';
 
+
+-- nsert the following data for vets:
+-- Vet William Tatcher is 45 years old and graduated Apr 23rd, 2000.
+-- Vet Maisy Smith is 26 years old and graduated Jan 17th, 2019.
+-- Vet Stephanie Mendez is 64 years old and graduated May 4th, 1981.
+-- Vet Jack Harkness is 38 years old and graduated Jun 8th, 2008.
+
+INSERT INTO vets(name, age, adate_of_graduation) VALUES('William Tatcher',45,'2000-04-23'),('Maisy Smith',26,'2019-01-17'),
+('Stephanie Mendez',64,'1981-05-04'),('Jack Harkness',38,'2008-01-8');
+
+
+
+-- Insert the following data for specialties:
+-- Vet William Tatcher is specialized in Pokemon.
+-- Vet Stephanie Mendez is specialized in Digimon and Pokemon.
+-- Vet Jack Harkness is specialized in Digimon.
+
+INSERT INTO specializations(species_id,vets_id) VALUES (1,1),(1,3),(2,3),(2,4);
+
+Insert the following data for visits:
+-- Agumon visited William Tatcher on May 24th, 2020.
+-- Agumon visited Stephanie Mendez on Jul 22th, 2020.
+-- Gabumon visited Jack Harkness on Feb 2nd, 2021.
+INSERT INTO visits VALUES(5,1,'2020-05-24'),(5,3,'2020-07-22'),(7,4,'2021-02-02'); 
+
+/* form here*/
+
+-- Pikachu visited Maisy Smith on Jan 5th, 2020.
+-- Pikachu visited Maisy Smith on Mar 8th, 2020.
+-- Pikachu visited Maisy Smith on May 14th, 2020.
+INSERT INTO visits VALUES(9,2,'2020-01-05'),(9,2,'2020-03-08'),(9,2,'2020-05-14');
+
+
+-- Devimon visited Stephanie Mendez on May 4th, 2021.
+-- Charmander visited Jack Harkness on Feb 24th, 2021.
+INSERT INTO visits VALUES(6,3,'2021-05-04'),(3,4,'2021-02-24');
+
+
+
+-- Plantmon visited Maisy Smith on Dec 21st, 2019.
+-- Plantmon visited William Tatcher on Aug 10th, 2020.
+-- Plantmon visited Maisy Smith on Apr 7th, 2021.
+INSERT INTO visits VALUES(1,2,'2019-12-21'),(1,1,'2020-08-10'),(1,2,'2021-04-07');
+
+
+-- Squirtle visited Stephanie Mendez on Sep 29th, 2019.
+-- Angemon visited Jack Harkness on Oct 3rd, 2020.
+-- Angemon visited Jack Harkness on Nov 4th, 2020.
+INSERT INTO visits VALUES(4,3,'2019-09-29'),(2,4,'2020-10-03'),(2,4,'2021-11-04');
+
+
+-- Boarmon visited Maisy Smith on Jan 24th, 2019.
+-- Boarmon visited Maisy Smith on May 15th, 2019.
+-- Boarmon visited Maisy Smith on Feb 27th, 2020.
+-- Boarmon visited Maisy Smith on Aug 3rd, 2020.
+INSERT INTO visits VALUES(8,2,'2019-01-24'),(8,2,'2019-05-15'),(8,2,'2021-02-27'),(8,2,'2020-08-03');
+
+-- Blossom visited Stephanie Mendez on May 24th, 2020.
+-- Blossom visited William Tatcher on Jan 11th, 2021.
+INSERT INTO visits VALUES(10,3,'2020-05-24'),(10,1,'2020-01-11');
